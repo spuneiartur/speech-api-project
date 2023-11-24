8a53bda0eff0223d90b5a09d74098d173357c5a6
Spunei Artur <arthurspunei@gmail.com>
2023-11-24 17:07:46 +0200

    added folder structure for the project

---
+++ controller/index.js
@@ -0,0 +1,9 @@
+`use strict`;
+import * as Model from "../model/index.js";
+
+const wrapper = document.querySelector(".wrapper");
+
+wrapper.textContent = ` ${Model.getResponseFromOpenAI()} - ${Model.getResponseFromSpeechAPI()}`;
+
+console.log(Model.getResponseFromSpeechAPI);
+console.log(Model.getResponseFromOpenAI);
---
+++ model/index.js
@@ -0,0 +1,9 @@
+`use strict`;
+
+export const getResponseFromOpenAI = () => {
+  return "This is a hard coded response from the OpenAI API";
+};
+
+export const getResponseFromSpeechAPI = () => {
+  return "This is a hard coded response from the speech API";
+};
