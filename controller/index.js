d88c453cd9327f22f0f500c56c0998f0522fc1b5
Spunei Artur <arthurspunei@gmail.com>
2023-11-26 15:37:10 +0200

    implemented app interface

--- controller/index.js
+++ controller/index.js
@@ -3,7 +3,7 @@ import * as Model from "../model/index.js";
 
 const wrapper = document.querySelector(".wrapper");
 
-wrapper.textContent = ` ${Model.getResponseFromOpenAI()} - ${Model.getResponseFromSpeechAPI()}`;
+// wrapper.textContent = ` ${Model.getResponseFromOpenAI()} - ${Model.getResponseFromSpeechAPI()}`;
 
-console.log(Model.getResponseFromSpeechAPI);
-console.log(Model.getResponseFromOpenAI);
+// console.log(Model.getResponseFromSpeechAPI);
+// console.log(Model.getResponseFromOpenAI);
