e76bdfb1cc108c07722b79b328aab974fc22043b
Spunei Artur <arthurspunei@gmail.com>
2024-01-03 19:59:54 +0200

    Update OPENAI_TOKEN and OPENAI_API_URL in constants.js

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-const OPENAI_TOKEN = "";
-const OPENAI_API_URL = "";
+const OPENAI_TOKEN = "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";
+const OPENAI_API_URL = "https://api.openai.com/v1/engines/davinci/completions";
 
 export { OPENAI_TOKEN, OPENAI_API_URL };
