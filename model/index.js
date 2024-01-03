9207d72b3d59a08ecc8c073f8de1d7e0a4d0ad75
Spunei Artur <arthurspunei@gmail.com>
2024-01-03 19:54:44 +0200

    Add .gitignore file with npm and project-specific files

--- model/index.js
+++ model/index.js
@@ -1,24 +1,27 @@
 `use strict`;
+import { OPENAI_TOKEN, OPENAI_API_URL } from "../constants.js";
 
 export const getResponseFromOpenAI = async (questionFromSpeechAPI) => {
-
- 
-  const response = await axios.post('https://api.openai.com/v1/engines/davinci/completions', {
-    prompt:'WHat is 2+2',
-    max_tokens: 60
-  }, {
-    headers: {
-      'Authorization': 'Bearer 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',
-      'Content-Type': 'application/json'
+  console.log(OPENAI_API_URL, OPENAI_TOKEN);
+  console.log("questionFromSpeechAPI", questionFromSpeechAPI);
+  const response = await axios.post(
+    `${OPENAI_API_URL}`,
+    {
+      prompt: "WHat is 2+2",
+      max_tokens: 60,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${OPENAI_TOKEN}}`,
+        "Content-Type": "application/json",
+      },
     }
-  });
-  
+  );
+
   return response.data.choices[0].text.trim();
-
-
-
 };
 
 export const getResponseFromSpeechAPI = () => {
   return "This is a hard coded response from the speech API";
 };
+getResponseFromOpenAI();
