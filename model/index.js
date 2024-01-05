307fd83aac9c6c83d3441a79403dc4396b3a2011
Zaremba Daniel <danutzaremba@gmail.com>
2024-01-05 20:47:09 +0200

    Add timeout for speech recognition

--- model/index.js
+++ model/index.js
@@ -27,6 +27,51 @@ export const getResponseFromSpeechAPI = () => {
   let recognizing = false;
   let finalTranscript = '';
   
+  // Get the canvas and its context
+  const canvas = document.getElementById('canvas');
+  const canvasCtx = canvas.getContext('2d');
+
+  // Create an audio context
+  const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+  // Create an analyser node
+  const analyser = audioCtx.createAnalyser();
+  analyser.fftSize = 2048;
+  const bufferLength = analyser.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
+
+  function draw() {
+    requestAnimationFrame(draw);
+
+    analyser.getByteTimeDomainData(dataArray);
+
+    canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+
+    canvasCtx.lineWidth = 2;
+    canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+    canvasCtx.beginPath();
+
+    const sliceWidth = canvas.width * 1.0 / bufferLength;
+    let x = 0;
+
+    for(let i = 0; i < bufferLength; i++) {
+      const v = dataArray[i] / 128.0;
+      const y = v * canvas.height / 2;
+
+      if(i === 0) {
+        canvasCtx.moveTo(x, y);
+      } else {
+        canvasCtx.lineTo(x, y);
+      }
+
+      x += sliceWidth;
+    }
+
+    canvasCtx.lineTo(canvas.width, canvas.height / 2);
+    canvasCtx.stroke();
+  }
+
   if ('webkitSpeechRecognition' in window) {
     recognition = new webkitSpeechRecognition();
     recognition.continuous = true;
@@ -56,7 +101,8 @@ export const getResponseFromSpeechAPI = () => {
       }
      
      document.querySelector('#chat__inputt .input').value=finalTranscript ;
-    
+     //const responseFromOpenAI = await getResponseFromOpenAI(finalTranscript);
+     //console.log(responseFromOpenAI);
      
     };
   } else {
@@ -70,6 +116,18 @@ export const getResponseFromSpeechAPI = () => {
     }
     recognition.lang = 'en-US';
     recognition.start();
+    navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+    .then(stream => {
+      const source = audioCtx.createMediaStreamSource(stream);
+      source.connect(analyser);
+      draw();
+      setTimeout(() => {
+        source.disconnect(analyser);
+        stream.getTracks().forEach(track => track.stop());
+      }, 5000);
+    })
+    .catch(err => console.error(err));
+
     //makes the speech recognition stop after 5 seconds
     setTimeout(() => recognition.stop(), 5000);
   };
