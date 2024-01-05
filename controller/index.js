f69c29e7b0895fb4960414d02a099304281c6a20
Spunei Artur <arthurspunei@gmail.com>
2024-01-05 20:30:28 +0200

    Update controller and view files

--- controller/index.js
+++ controller/index.js
@@ -1,15 +1,19 @@
 `use strict`;
 import * as Model from "../model/index.js";
-
+import * as View from "../view/view.js";
 const wrapper = document.querySelector(".wrapper");
 
- //wrapper.textContent = ` ${Model.getResponseFromOpenAI()} - ${Model.getResponseFromSpeechAPI()}`;
+//wrapper.textContent = ` ${Model.getResponseFromOpenAI()} - ${Model.getResponseFromSpeechAPI()}`;
 //console.log(Model.getResponseFromOpenAI);
 //console.log(Model.getResponseFromSpeechAPI);
-(async () => {
-    try {
-        const responseFromSpeechAPI = await Model.getResponseFromSpeechAPI();
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  })();
+// (async () => {
+//   try {
+//     const responseFromSpeechAPI = await Model.getResponseFromSpeechAPI();
+//   } catch (error) {
+//     console.error("Error:", error);
+//   }
+// })();
+console.log("gegeagea");
+View.generateChatUser("Hello from user");
+View.generateChatAI("Hello from AI");
+View.generateChatUser("How are you");
